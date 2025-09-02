59d232d245fed7218c95b2c64794e6e242643caa
Felipe Santos <tfelipesantos99s@gmail.com>
2025-09-02 13:12:46 -0300

    Update to allow creation of new games

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
-from models import Jogo
+from models.game import Game
 
 app = Flask(__name__)
+
+game1= Game('Tetris', 'Puzzle', 'Atari')
+game2= Game('God of War', 'Rack n Slash', 'PS2')
+gameList = [game1, game2]
 
 @app.route('/index')
 @app.route('/')
 def home():
-    jogo1= Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2= Jogo('God of War', 'Rack n Slash', 'PS2')
-    lista = [jogo1, jogo2]
-    return render_template('index.html', title="Jogos", jogos=lista)
+    return render_template('index.html', title="Jogos", games=gameList)
 
-app.run()
+@app.route('/form-new-game')
+def form_new_game():
+    return render_template('form-new-game.html', title="Adicionar novo jogo")
+
+@app.route('/add-game', methods=['POST',])
+def add_game():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+    game = Game(name, category, console)
+    gameList.append(game)
+    return redirect('/')
+
+app.run(debug=True)
